f828f0eec2f8529bd46e22842503da1ad0f767d3
Chidinma Nwatu <chidinma.nwatu@sproxil.com>
2026-02-25 14:16:32 +0100

    fixed cors error

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import authRoutes from './routes/auth.routes';
 import productRoutes from './routes/product.routes';
 import saleHistoryRoutes from './routes/saleHistory.routes';
 import globalErrorHandler from './middlewares/globalErrorHandler';
+import cors from "cors";
 
 const app: Application = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
